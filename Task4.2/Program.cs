edb578c9f72c1a0e1009efba9b7aa5f5c3536258
miniPigasus404 <167249973+miniPigasus404@users.noreply.github.com>
2024-05-03 23:57:05 +0300

    Add files via upload

---
+++ Task4.2/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Task4._2
+{
+    struct Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Category { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Price}";
+        }
+    }
+    internal class Program
+    {
+        static void TotalPrice(ILookup<string, Product> lookup)
+        {
+            int sum = 0;
+            foreach (var product in lookup)
+            {
+                foreach (var el in product)
+                {
+                    Console.WriteLine(el.ToString());
+                    sum += el.Price;
+                }
+                Console.WriteLine($"{product.Key} {sum}");
+                sum = 0;
+            }
+        }
+        static void Main(string[] args)
+        {
+            var products = new List<Product>();
+            products.Add(new Product { Name = "SmartTV", Price = 400, Category = "Electronics" });
+            products.Add(new Product { Name = "Lenovo ThinkPad", Price = 1000, Category = "Electronics" });
+            products.Add(new Product { Name = "Iphone", Price = 700, Category = "Electronics" });
+            products.Add(new Product { Name = "Orange", Price = 2, Category = "Fruits" });
+            products.Add(new Product { Name = "Banana", Price = 3, Category = "Fruits" });
+            ILookup<string, Product> lookup = products.ToLookup(prod => prod.Category);
+            TotalPrice(lookup);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Task5/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Task5
+{
+    class ParallelUtils<T, TR>
+    {
+        private T operand1;
+        private T operand2;
+        private Func<T, T, TR> operation;
+
+        public ParallelUtils(T operand1, T operand2, Func<T, T, TR> operation)
+        {
+            this.operand1 = operand1;
+            this.operand2 = operand2;
+            this.operation = operation;
+        }
+
+        public TR Result { get; private set; }
+
+        public Task StartEvaluation()
+        {
+            return Task.Run(() => this.Result = operation(operand1, operand2));
+        }
+
+        public async Task Evaluation()
+        {
+            this.Result = await Task.Run(() => operation(this.operand1, this.operand2));
+        }
+
+
+    }
+    internal class Program
+    {
+        static double addiotion(int a, int b)
+        {
+            return a + b;
+        }
+        static double substraction(int a, int b)
+        {
+            return a - b;
+        }
+        static async Task Main(string[] args)
+        {
+            int op1 = 10;
+            int op2 = 5;
+            ParallelUtils<int, double> ex1 = new ParallelUtils<int, double>(op1, op2, addiotion);
+            ParallelUtils<int, double> ex2 = new ParallelUtils<int, double>(op1, op2, substraction);
+
+            ex1.Evaluation();
+            ex1.StartEvaluation();
+
+            ex2.Evaluation();
+            ex2.StartEvaluation();
+
+            Task.WaitAll();
+            Console.WriteLine($"Ex1 Res: {ex1.Result}, Ex2 Res: {ex2.Result}");
+        }
+    }
+}
